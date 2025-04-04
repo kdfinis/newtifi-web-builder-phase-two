104e740bd101fb931cc71ab15f3df64b70ededd2
Karlo Definis <karlodefinis@gmail.com>
2025-04-04 15:11:37 +0200

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/newtifi-web-builder-phase-two/' : '/',
+export default defineConfig({
+  base: '/newtifi-web-builder/',
   server: {
-    host: "::",
-    port: 8080,
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      host: '0.0.0.0',
+      port: 3000
+    }
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
