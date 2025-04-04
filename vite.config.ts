307e7bb30a9f53c5264d15dd7e448d207f0c2473
Karlo Definis <karlodefinis@gmail.com>
2025-04-04 23:38:49 +0200

    Restore to simplified working configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,29 +5,13 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/newtifi-web-builder-phase-two/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      }
-    }
-  },
   server: {
-    host: true,
-    port: 8080,
-    strictPort: false,
-    open: true
+    port: 3000
   },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  publicDir: 'public',
+  }
 });
