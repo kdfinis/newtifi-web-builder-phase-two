288b32f3fcc7e64ac5c9a3abd0d06557ed83193c
Karlo Definis <contact@coad.ai>
2026-01-21 22:46:46 +0100

    Updates

---
+++ assets/user-BFn9ZXS4-BOLGgfMA.js
@@ -0,0 +1,7 @@
+import{X as r}from"./index-C5NeN4KH.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=r("User",[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]]);export{c as r};
+//# sourceMappingURL=user-BFn9ZXS4-BOLGgfMA.js.map
