57996289e316884e432165dd7e0982cc32e149c9
Karlo Definis <contact@coad.ai>
2026-01-23 11:05:40 +0100

    Updates

--- assets/arrow-right-DjUfA6cj-Ns08XU_8-KXHY6cmk-DM4ui72v.js
+++
@@ -1,7 +0,0 @@
-import{Y as o}from"./index-BHB10gxo.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const h=o("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]);export{h as o};
-//# sourceMappingURL=arrow-right-DjUfA6cj-Ns08XU_8-KXHY6cmk-DM4ui72v.js.map
